e545f0abc1bada0ec836318a7be79d11e98703b7
Aydar <me@aydar.media>
2024-10-30 04:13:01 +0300

    oh

--- app/controllers/common_pages_controller.rb
+++ app/controllers/common_pages_controller.rb
@@ -3,5 +3,13 @@ class CommonPagesController < ApplicationController
 		@page_nav_name = 'home'
 	end
 
+	def promo
+		@page_nav_name = 'promo'
+	end
+
+	def about
+		@page_nav_name = 'about'
+	end
+
 	def search; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 	root 'common_pages#index'
 
 	get '/search', to: 'common_pages#search'
+	get '/promo', to: 'common_pages#promo'
+	get '/about', to: 'common_pages#about'
 	get '/profile', to: 'users#single'
 	get '/featured', to: 'lifetimes#featured'
 	get '/lifetime/:id/(:year)', to: 'lifetimes#single', as: 'single_lifetime'
