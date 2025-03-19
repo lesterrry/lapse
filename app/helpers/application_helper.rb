8cd08d92a801c1b15f674a0324cfdff5ec8d150f
Aydar <me@aydar.media>
2025-03-19 16:14:44 +0300

    qualify helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def cn(*classes)
         classes.join(' ')
     end
+
+    def qualify(entity, *additional)
+        entity_name = entity.class.name.downcase
+        entity_id = entity.id
+
+        all = [entity_name, entity_id, *additional]
+        all.join('_')
+    end
 end
